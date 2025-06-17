486a5e9617dee6b3dc9519bc89c915bf5eb57ed1
guillermo reyes <guillermo.reyes@transcat.com>
2025-06-17 16:03:01 -0700

    Simple Home

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+export default function Home() {
+    return (
+        <div className="Home">          
+            <div className="Home__header">
+                <h1>Transcat Tools</h1>
+            </div>
+            <div className="Home__content">
+                <p>Welcome to Transcat Tools!</p>
+                <p>This is a simple React application.</p>
+            </div>
+        </div>
+    )
+}
