e797e9691f52cdbe80fdc5beb6501878f335756e
guillermo reyes <guillermo.reyes@transcat.com>
2025-06-17 15:55:13 -0700

    Added proxy to avoid CORS requests in dev

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// Bread: Added Proxy to avoid CORS issues during development
+// At the SPA level we just call /api/whatever and that will map it to this proxy
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5100',
+        changeOrigin: true
+      }
+    }
+  },
 })
+
+
