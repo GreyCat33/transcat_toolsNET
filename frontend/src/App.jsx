c1688ee86567973e6a9ca2084516e70b8b9adf04
guillermo reyes <guillermo.reyes@transcat.com>
2025-06-17 16:02:37 -0700

    Routes in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,25 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Users from './pages/Users'
 import NotFoundPage from './pages/NotFoundPage'
+import Nav from './components/Nav'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <NotFoundPage />
-    </>
+  <BrowserRouter>
+    <Nav />
+
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/users" element={<Users />} />
+  
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  </BrowserRouter>
   )
 }
 
