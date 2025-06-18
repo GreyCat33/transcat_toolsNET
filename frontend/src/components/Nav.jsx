2be95eebe5d5611b7b2e4346d7c3111a8567ff34
guillermo reyes <guillermo.reyes@transcat.com>
2025-06-17 17:58:43 -0700

    Navbar styles to a close approximation to original

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,38 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import "./Nav.css"; // Assuming you have a CSS file for styling
 export default function Nav() {
     return (
         <nav>
-        <div>
-            <a>
-            Transcat Tools
-            </a>
-            <button
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-            >
-            <span ></span>
-            </button>
-            <div  id="navbarNav">
-            <ul>
-                <li>
-                <Link aria-current="page" to="/">
-                    Home
+            <div className="Nav-Right-Container">
+                <Link to="/" className="navbar-brand">
+                    <img src="./src/assets/logo.png" alt="Logo" className="logo" width={"150px"} style={{objectFit:"contain", padding:"0 20px"}}/>
                 </Link>
-                </li>
-                <li>
-                <Link to="/users">
-                    Users
-                </Link>
-                </li>
-            </ul>
+                
+                <div id="navbarNav">
+                    <ul className="navLinks-container">
+                        <li>
+                            <Link className="navbar-link" aria-current="page" to="/">
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link className="navbar-link"  to="/users">
+                                Users
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
         </nav>
     );
-    }
+}
