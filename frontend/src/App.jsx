4733c979cf59552ca30aceb694eab97a587b6779
guillermo reyes <guillermo.reyes@transcat.com>
2025-06-19 19:12:59 -0700

    Since we elevated our Browser Router component up one level we had to do some clean up at app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,29 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Users from './pages/Users'
 import NotFoundPage from './pages/NotFoundPage'
 import Nav from './components/Nav'
 import './App.css'
 
-function App() {
-  
 
+
+function App() {
   return (
-  <BrowserRouter>
+    <>
+    
     <Nav />
 
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/users" element={<Users />} />
-  
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
-  </BrowserRouter>
+      <div style={{ padding: '20px', display: 'flex', justifyContent: 'center' }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/users" element={<Users />} />
+
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
+    </>
+
   )
 }
 
