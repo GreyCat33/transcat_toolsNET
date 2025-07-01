3267ef5f9f6a1bd255b340ef276d8e34b9d72911
guillermo reyes <guillermo.reyes@transcat.com>
2025-07-01 08:54:39 -0700

    CRUD for the user table in DB

--- Controller/UserController.cs
+++ Controller/UserController.cs
@@ -23,4 +23,37 @@ public class UsersController : ControllerBase
         await _db.SaveChangesAsync();
         return CreatedAtAction(nameof(Get), new { id = user.Id }, user);
     }
-}
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        var user = await _db.Users.FindAsync(id);
+        if (user == null)
+            return NotFound();  
+        return Ok(user);    
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(int id, [FromBody] User user)
+    {
+        if (id != user.Id)
+            return BadRequest("User ID mismatch");
+
+        _db.Users.Update(user);
+        await _db.SaveChangesAsync();
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var user = await _db.Users.FindAsync(id);
+        if (user == null)
+            return NotFound();
+
+        _db.Users.Remove(user);
+        await _db.SaveChangesAsync();
+        return NoContent();
+    }  
+
+} 
