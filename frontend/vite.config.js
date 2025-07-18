4bea50f2537c6d77b6b1d3f93277834414f05699
guillermo reyes <guillermo.reyes@transcat.com>
2025-07-18 09:59:02 -0700

    vite config to include a alias for root path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { resolve }     from 'path';
 // https://vite.dev/config/
 // Bread: Added Proxy to avoid CORS issues during development
 // At the SPA level we just call /api/whatever and that will map it to this proxy
+// the command below only works for dev, proxy dropped when building app
 export default defineConfig(({ command }) => ({
 
   root: "./",
@@ -15,7 +16,11 @@ export default defineConfig(({ command }) => ({
       input: "./index.html",  
     }
   },
-
+resolve:{
+  alias:{
+    "~": resolve(__dirname,"src")
+  }
+},
   //Only add the proxy if we are running the dev server
   ...(command === 'serve' && {
     server: {
